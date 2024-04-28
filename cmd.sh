2c7e8eeb03d2e285fc8f36f700d09504c9f97c4d
MRAzaly20 <muhammadrazalyy@gmail.com>
2024-04-28 11:42:39 +0700

    initial

---
+++ cmd.sh
@@ -0,0 +1,7 @@
+echo "# Salon-Appoinments-Scheduler" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MRAzaly20/Salon-Appoinments-Scheduler.git
+git push -u origin main
